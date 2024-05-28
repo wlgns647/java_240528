8589c6fe0c4f2b6d028014c46b6290ff6c3c080c
wlgns647 <wlgns64711234@gmail.com>
2024-05-28 17:11:05 +0900

    1일차 변수 선언예제

---
+++ java_kgh/src/day01/VeriableEx01.java
@@ -0,0 +1,28 @@
+package day01;
+
+public class VeriableEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char ch1; //문자형 변수인 ch1 선언 
+		char ch2;
+		ch2='A';
+		
+		char ch3,ch4;  //같은 자료형인 경으 , 를 통해 동시선언 가능
+		
+		char ch5='+',ch6='\n';
+		
+		System.out.println(ch2);
+		System.err.println(ch6);
+		System.err.println(ch5);
+		
+		
+		//문자형 변수 ch5를 선언하고 +로 초기화
+		
+		long num = 1234567890123l;
+		
+		
+	}
+
+}
