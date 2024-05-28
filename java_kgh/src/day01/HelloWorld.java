02502c411554a6452ae0fdf7878a9907884015d9
wlgns647 <wlgns64711234@gmail.com>
2024-05-28 17:10:50 +0900

    자바 1일차 HelloWorld예제

---
+++ java_kgh/src/day01/HelloWorld.java
@@ -0,0 +1,22 @@
+package day01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) { //프로그렘이 실행되기 위해서 main이 필요하다
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world"); //콘솔에 원하는 문구를 출력하는 기능 
+		/* 
+		 * 
+		 * 사이 주석 
+		 */
+		/** 메서드 설명을 위한 주석 **/
+		 
+		 
+	
+
+
+		
+	}
+
+}
