1da73bbf660fdfecb776d9faf0f45c5008b88a25
wlgns647 <wlgns64711234@gmail.com>
2024-05-29 23:54:28 +0900

    집에서 조금 하다멈춤

---
+++ java_kgh/src/day02re/All.java
@@ -0,0 +1,16 @@
+package day02re;
+
+import java.util.Scanner;
+
+public class All {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+		
+		
+		
+	}
+
+}
