85c16e676a4d4ab81333e55c11dabe2f597ddf73
wlgns647 <wlgns64711234@gmail.com>
2024-05-30 17:48:56 +0900

    집에서 연습할 페키지

---
+++ java_kgh/src/home/Start.java
@@ -0,0 +1,19 @@
+package home;
+
+import java.util.Scanner;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		Scanner scan = new Scanner(System.in);
+		String a1 = scan.next();
+		System.out.println("안녕하세요" );
+		
+		
+		
+		
+	}
+
+}
