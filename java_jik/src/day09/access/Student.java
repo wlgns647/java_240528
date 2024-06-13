dfdc99e6ebbc5fe63b69590c9052639ddc9390da
wlgns647 <wlgns64711234@gmail.com>
2024-06-14 01:45:12 +0900

    d

--- java_jik/src/day09/access/Student.java
+++
@@ -1,8 +0,0 @@
-package day09.access;
-
-public class Student {
-	public int grade;
-	protected int classNum;
-	int num;//default
-	private String name;
-}
---
+++ java_kgh/src/home/Hi.java
@@ -0,0 +1,16 @@
+package home;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int []arr1 = new int[] { 1,7,3,4,5,};
+		Arrays.sort(arr1); 
+		System.out.println(Arrays.toString(arr1));
+	}
+
+}
