f250e5483fec12ba6b633e0691038bbf58e47ad8
wlgns647 <wlgns64711234@gmail.com>
2024-08-26 10:48:38 +0900

    dd

--- servlet/servlet3/src/main/java/kr/kh/app/controller/Signup.java
+++ servlet/servlet3/src/main/java/kr/kh/app/controller/Signup.java
@@ -1,7 +1,6 @@
 package kr.kh.app.controller;
 
 import java.io.IOException;
-import java.lang.reflect.Member;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,18 +9,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kr.kh.app.model.vo.MemberVO;
+import kr.kh.app.service.MemberService;
+import kr.kh.app.service.MemberServiceImp;
 
 @WebServlet("/signup")
 public class Signup extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private MemberService memberService = new MemberServiceImp();
+	
     public Signup() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
 		
 		request.getRequestDispatcher("WEB-INF/views/member/signup.jsp").forward(request, response);
 	}
@@ -32,9 +32,19 @@ public class Signup extends HttpServlet {
 		String me_pw = request.getParameter("me_pw");
 		String me_email = request.getParameter("me_email");
 		
-		MemberVO mamber = new MemberVO(me_id, me_pw, me_email);
+		MemberVO member = new MemberVO(me_id, me_pw, me_email);
 		
-		doGet(request, response);
+		
+		if(memberService.signup(member)) {
+			request.setAttribute("msg", "회원가입을 완료했습니다");
+			request.setAttribute("url", "/");
+		}else {
+			request.setAttribute("msg", "회원가입을 실패했습니다");
+			request.setAttribute("url", "/signup");
+		}
+		
+		request.getRequestDispatcher("WEB-INF/views/member/signup.jsp").forward(request, response);
+
 	}
 
 }
--- servlet/servlet3/src/main/java/kr/kh/app/service/MemberServiceImp.java
+++ servlet/servlet3/src/main/java/kr/kh/app/service/MemberServiceImp.java
@@ -8,9 +8,12 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import kr.kh.app.dao.MemberDAO;
+import kr.kh.app.model.vo.MemberVO;
 
 public class MemberServiceImp implements MemberService  {
 
+	private MemberDAO memberDao;
+	
 	public MemberServiceImp() {
 		String resource = "kr/kh/app/config/mybatis-config.xml";
 		InputStream inputStream;
@@ -23,6 +26,22 @@ public class MemberServiceImp implements MemberService  {
 			memberDao = session.getMapper(MemberDAO.class);
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean signup(MemberVO member) {
+		//member null 체크
+		
+		//아이디 유효성 검사
+		
+		//비번 유효성 검사
+		
+		try {
+			return memberDao.insertMember(member);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return false;
 		}
 	}
 }
