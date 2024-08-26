08a4205a135edb9587b13abc297ca3004dcec154
wlgns647 <wlgns64711234@gmail.com>
2024-08-26 15:58:13 +0900

    긴급복구

--- spring/spring1/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring/spring1/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
 
 import kr.kh.spring.model.dto.PersonDTO;
 import kr.kh.spring.service.MemberService;
